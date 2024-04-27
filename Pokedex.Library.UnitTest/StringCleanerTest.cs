614eabb16de728dc68a7c2ffeffb11e59b7a0409
Federico Pessina <federicopessina.work@gmail.com>
2024-04-27 23:17:28 +0200

    Add test project

---
+++ Pokedex.Library.UnitTest/StringCleanerTest.cs
@@ -0,0 +1,24 @@
+﻿using Pokedex.Library.Utilities;
+
+namespace Pokedex.Library.UnitTest
+{
+    public class StringCleanerTest
+    {
+        [Theory]
+        [InlineData("This is a test string\n", "This is a test string ")]
+        [InlineData("This is a test string\f", "This is a test string ")]
+        [InlineData("This is a test string\r", "This is a test string ")]
+        [InlineData("This is a test string\t", "This is a test string ")]
+        public void RemoveEscapeSequencesTest(string input, string expected)
+        {
+            // Arrange
+            string actual;
+
+            // Act
+            actual = StringCleaner.RemoveEscapeSequences(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
